615bd999c1f90d30b297dab17dd6387ee78c8e3a
James Brechtel <jbrechtel@gmail.com>
2012-10-13 10:16:59 -0300

    Add states definition

--- src/clojoomba/room_generation.clj
+++ src/clojoomba/room_generation.clj
@@ -5,5 +5,15 @@
                      (fn [_] (= 1 (rand-int 2)))
                      (range size))))
 
-(defn room [size] (vec
-                    (map row (repeat size size))))
+(defn gen-room [size] (vec
+                        (map row (repeat size size))))
+
+(def possible-states [:clean :dirty :wall])
+
+(def state-permutations (for [north possible-states,
+                              south possible-states,
+                              east  possible-states,
+                              west  possible-states,
+                              curr  possible-states] (list north south east west curr)))
+
+(def states (zipmap (vec state-permutations) (range (count state-permutations))))
