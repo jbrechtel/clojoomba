26a1373e09306afb0a65db533d3da06ebade18bc
James Brechtel <jbrechtel@gmail.com>
2012-10-27 19:39:22 -0300

    Time entire run, don't get fooled by laziness

--- src/clojoomba/core/runner.clj
+++ src/clojoomba/core/runner.clj
@@ -3,14 +3,18 @@
      clojoomba.core.evolution
      clojoomba.core.generation])
 
+(declare run-for)
+
 
 (defn -main [generations-str & args]
-  (let [generations (Integer/parseInt generations-str)
-        best-generation (last (take generations (evolutions {:agents (gen-agents 200) :steps 200 :room-size 10 :num-rooms 100})))
+  (let [generations (Integer/parseInt generations-str)]
+       (time (run-for generations))))
+
+(defn run-for [generations]
+  (let [best-generation (last (take generations (evolutions {:agents (gen-agents 200) :steps 200 :room-size 10 :num-rooms 100})))
         rooms (gen-rooms 100 10)
         scored (score-agents best-generation rooms 200)
         sorted (sort #(> (last %) (last %2)) scored)
         best (first (first sorted))
         best-score (last (first sorted))]
-    (println best-score)
-    (println best)))
+    (println best-score)))
