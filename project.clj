b825d039bbfacea2ee4b6be219086a08e70ed451
James Brechtel <jbrechtel@gmail.com>
2012-11-18 12:50:22 -0600

    Update project to latest clojure version, duh, add criterium and description

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clojoomba "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Robby the Robot implementation in Clojure"
   :main clojoomba.core.runner
   :aot [clojoomba.core.runner]
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [criterium "0.3.0"]]
   )
